3534910287c55c944a2c4e4b02502ac4807a4a11
Remco-Bruinsma <82671724+Remco-Bruinsma@users.noreply.github.com>
2024-03-20 14:58:45 +0100

    changed enviroment variable

--- src/Enviromental Variables/APIConnection.js
+++ src/Enviromental Variables/APIConnection.js
@@ -1,9 +1,10 @@
 export const ApiConnectionReplacement = () => {// Initialize state with an initial value
-    if (process.env.REACT_APP_APICONNECTION !== undefined) {
+    if (process.env.APICONNECTION !== undefined) {
         console.log(process.env.APICONNECTION);
-        return (process.env.REACT_APP_APICONNECTION); // Update the state value using the setUrl function
+        return (process.env.APICONNECTION); // Update the state value using the setUrl function
     }
     else {
+        console.log(process.env.APICONNECTION);
         console.log("https://localhost:44303")
         return ("https://localhost:44303");
     } // Return the current state value
